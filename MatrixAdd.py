4e765548affe75803153c82f4d33f480a2243fc4
sakthiprasad2004 <125357004+sakthiprasad2004@users.noreply.github.com>
2024-03-05 20:01:44 +0530

    Update MatrixAdd.py

--- MatrixAdd.py
+++ MatrixAdd.py
@@ -4,9 +4,10 @@ m1 = [[1,2,3],[4,5,6]]
 m2 = [[1,2,3],[4,5,6]]
 
 m3 = list()
-
+def change():
 for i in m1:
 	for j in m2:
 		m3.append(i+j) 
+	return m3
 
-print("sum is : ", m3)
+print(change(m3))
